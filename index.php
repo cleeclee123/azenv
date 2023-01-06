774611b89379a9e7703a1ab9bed05869df8b26e7
Chris Lee <cl114@illinois.edu>
2023-01-05 20:16:45 -0600

    to json

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-
-<head>
-	<title>AZ Environment variables 1.04</title>
-</head>
-
-<body>
-<pre>
 <?php
 ##########################################################################
 #	
@@ -34,7 +25,44 @@
 // 	}
 // }
 
-// Additions/Modifications Below
+// Additions/Modifications Below:
+
+/**
+ *  An example CORS-compliant method.  It will allow any GET, POST, or OPTIONS requests from any
+ *  origin.
+ *
+ *  In a production environment, you probably want to be more restrictive, but this gives you
+ *  the general idea of what is involved.  For the nitty-gritty low-down, read:
+ *
+ *  - https://developer.mozilla.org/en/HTTP_access_control
+ *  - https://fetch.spec.whatwg.org/#http-cors-protocol
+ * 
+ * Reference: https://stackoverflow.com/questions/8719276/cross-origin-request-headerscors-with-php-headers
+ */
+function cors()
+{
+	// Allow from any origin
+	if (isset($_SERVER['HTTP_ORIGIN'])) {
+		// Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
+		// you want to allow, and if so:
+		header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+		header('Access-Control-Allow-Credentials: true');
+		header('Access-Control-Max-Age: 86400');    // cache for 1 day
+	}
+
+	// Access-Control headers are received during OPTIONS requests
+	if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+		if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+			// may also be using PUT, PATCH, HEAD etc
+			header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
+		if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+			header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+		exit(0);
+	}
+}
 
 /**
  * Get all HTTP header key/values as an associative array for the current request.
@@ -58,8 +86,7 @@ function getRequestHeaders()
 			if (!isset($copy_server[$key]) || !isset($_SERVER[$key])) {
 				$headers[$key] = $value;
 			}
-		}		
-		elseif (isset($copy_server[$key])) {
+		} elseif (isset($copy_server[$key])) {
 			$headers[$copy_server[$key]] = $value;
 		}
 	}
@@ -78,11 +105,10 @@ function getRequestHeaders()
 	return $headers;
 }
 
-// to json
-$jsonRes = json_encode(getRequestHeaders());
-echo ($jsonRes);
-?>
+// enable cors 
+cors();
 
-</pre>
-</body>
-</html>
+// to json
+header('Content-Type: application/json; charset=utf-8');
+echo json_encode(getRequestHeaders());
+?>
